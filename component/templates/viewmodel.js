d025dca5eb909ab7dc3a27be8e4c1607fc2959ba
David Fullmer <david.d.fullmer@gmail.com>
2015-03-24 10:52:29 -0400

    updated component template to be more d3js friendly

--- component/templates/viewmodel.js
+++ component/templates/viewmodel.js
@@ -1,12 +1,45 @@
-define(['knockout', 'text!./<%= filename %>.html'], function(ko, templateMarkup) {
+define(['jquery', 'knockout', 'd3', 'text!./<%= filename %>.html'], function($, ko, d3, templateMarkup) {
 
 	function <%= viewModelClassName %> (params, componentInfo) {
-		this.message = ko.observable('Hello from the <%= name %> component!');
+		var self = this;
+		self.element = componentInfo.element;
+		self.firstRender = ko.observable(true)
+
+		self.data = params.data
+
+		self.header = ko.observable('<%= name %>');
+		self.message = ko.observable('Hello from the <%= name %> component!');
+
+		// list variable common to both render() and update()
+		self.svg = null;
+
+		//debugger;
+
+		self.render = function() {
+
+		}
+		
+		self.update = function() {
+
+		}
+
+		self.reactor = ko.computed(function() {
+			var data = self.data();
+			//debugger;
+			if (typeof data.data !== 'undefined') {
+				if (self.firstRender()) {
+					self.render()
+				} else {
+					self.update()
+				}
+			}
+			return data;
+		})
 	}
 
 	// This runs when the component is torn down. Put here any logic necessary to clean up,
 	// for example cancelling setTimeouts or disposing Knockout subscriptions/computeds.
-	<%= viewModelClassName %> .prototype.dispose = function() {};
+	<%= viewModelClassName %>.prototype.dispose = function() {};
 
 	return {
 		viewModel: {
