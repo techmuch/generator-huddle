0bfbb3c11bd1bd3474ab14360e56f1333291502f
David Fullmer <david.d.fullmer@gmail.com>
2015-06-29 12:16:10 -0400

    1. fixed conflict from having two instaneces of the same component on the screen at the same time

--- component/templates/viewmodel.js
+++ component/templates/viewmodel.js
@@ -1,12 +1,17 @@
 define(['knockout', 'text!./<%= filename %>.html'], function(ko, templateMarkup) {
 
 	function <%= viewModelClassName %> (params, componentInfo) {
-		var self = this;
-		
-		self.header = ko.observable('<%= name %>');
-		self.message = ko.observable('Hello from the <%= name %> component!');
+		var vm = function(params, componentInfo){
+			var self = this;
+			
+			self.header = ko.observable('<%= name %>');
+			self.message = ko.observable('Hello from the <%= name %> component!');
 
-		return self;
+			return self;
+
+		}
+
+		return new vm(params)
 	}
 
 	// This runs when the component is torn down. Put here any logic necessary to clean up,
